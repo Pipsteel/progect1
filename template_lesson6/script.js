050601ba4c3623e67640ad31639f383a8ddad84a
Pipsteel <55404259+Pipsteel@users.noreply.github.com>
2019-11-10 21:00:18 +0300

    end

---
+++ template_lesson6/script.js
@@ -0,0 +1,13 @@
+//document .getElementsByClassName("cont8");
+//document .getElementById("")
+//document .querySelector
+//document .querySelectorALL
+
+document .getElementById("burger").onclick = function(){
+    addmenu();
+}
+
+function addmenu(){
+    document .getElementById("menu").classList.toggle("show");
+    
+}
